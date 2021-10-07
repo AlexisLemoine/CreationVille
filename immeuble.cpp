35dc4e0e491543f50c2e6411062e4a775fdc77f3
alexis <alexis.lemoine@etu.univ-lyon1.fr>
2021-10-07 13:50:59 +0200

    inserer des points

--- immeuble.cpp
+++ immeuble.cpp
@@ -22,21 +22,35 @@ void immeuble::etage (float x, float y, float z, float lx, float lz, LCC& lcc) {
     ib.add_vertex(Point(x+lx , y+1 , z+lz));
     ib.begin_surface();
     //on créé les faces du cube
-    Dart_handle dh1 = ib.add_facet({0,1,3,2});
+    Dart_handle dh1 = ib.add_facet({0,1,3,2}); // quand on appelle dh1, on est sur l'arrête (0, 1). arrête au même endroit pour les autres dh.
     Dart_handle dh2 = ib.add_facet({4,6,7,5});
     Dart_handle dh3 = ib.add_facet({1,0,4,5});
     Dart_handle dh4 = ib.add_facet({6,2,3,7});
     Dart_handle dh5 = ib.add_facet({2,6,4,0});
     Dart_handle dh6 = ib.add_facet({3,1,5,7});
 
-    lcc.insert_point_in_cell<1>(dh4,Point(x + 0.2, y, z + lz));
+    // Pour insérer les points sur les surfaces et créer des murs
+    /*lcc.insert_point_in_cell<1>(dh4,Point(x + 0.2, y, z + lz));
     lcc.insert_point_in_cell<1>(dh4,Point(x + lx - 0.2, y, z + lz));
     lcc.insert_point_in_cell<1>(dh2,Point(x + lx, y, z + 0.2));
     lcc.insert_point_in_cell<1>(dh2,Point(x + lx, y, z + lz - 0.2));
     lcc.insert_point_in_cell<1>(lcc.beta(dh3, 1), Point(x + 0.2, y, z));
     lcc.insert_point_in_cell<1>(lcc.beta(dh3, 1),Point(x + lx - 0.2, y, z));
     lcc.insert_point_in_cell<1>(lcc.beta(dh1, 0),Point(x, y, z + 0.2));
-    lcc.insert_point_in_cell<1>(lcc.beta(dh1, 0),Point(x, y, z + lz - 0.2));
+    lcc.insert_point_in_cell<1>(lcc.beta(dh1, 0),Point(x, y, z + lz - 0.2)); */
+
+    // lcc.insert_point_in_cell<1>(dh4,Point(x + 0.2, y + 1, z + lz));
+    // lcc.insert_point_in_cell<1>(dh4,Point(x + lx - 0.2, y + 1, z + lz));
+    // lcc.insert_point_in_cell<1>(dh2,Point(x + lx, y + 1, z + 0.2));
+    lcc.insert_point_in_cell<1>(dh2,Point(x + lx, y + 1, z + lz - 0.2));
+    // lcc.insert_point_in_cell<1>(lcc.beta(dh3, 1), Point(x + 0.2, y + 1, z));
+    // lcc.insert_point_in_cell<1>(lcc.beta(dh3, 1),Point(x + lx - 0.2, y + 1, z));
+    // lcc.insert_point_in_cell<1>(lcc.beta(dh1, 0),Point(x, y + 1, z + 0.2));
+    lcc.insert_point_in_cell<1>(lcc.beta(dh1, 0),Point(x, y + 1, z + lz - 0.2));
+
+
+    Dart_handle dh7 = lcc.insert_cell_0_in_cell_1(dh6);
+    CGAL_assertion( lcc.is_valid() );
     //  const Point& i0=(Point(x , y , z));
     //  const Point& i1=(Point(x+lx , y , z));
     //  const Point& i2=(Point(x+lx , y , z+lz));
